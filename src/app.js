3d5eea08302273e871af733b19c744e83d946feb
Balázs Réka <balazsreka@ktch.hu>
2023-02-02 20:18:26 +0100

    Típusok átnézve

---
+++ src/app.js
@@ -0,0 +1,28 @@
+var szam = '35';
+
+var szam2 ='27';
+
+
+console.log(Number(szam) + Number(szam2));
+
+var esik = true;
+if (esik == true) {
+    console.log("esik...")
+}
+
+var  calcArea  = function(base, height){
+return base * height /2;
+
+}
+console.log (typeof calcArea)
+
+var Éva = {
+    nev: 'Évi',
+    kor: '28',
+    van_kocsi: true
+}
+console.log (typeof Éva)
+
+var szamok = [25, 30, 40];
+console.log(typeof szamok);
+var nevek = ["Kata","Pali","Iván","Dani"];
